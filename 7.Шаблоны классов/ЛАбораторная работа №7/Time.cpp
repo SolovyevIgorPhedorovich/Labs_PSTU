5476b1b25b3779e016e197eeedb5cfa4f458fe04
SolovyevIgorPhedorovich <65609096+SolovyevIgorPhedorovich@users.noreply.github.com>
2020-05-19 21:49:37 +0500

    Add files via upload

---
+++ 7.Шаблоны классов/ЛАбораторная работа №7/Time.cpp
@@ -0,0 +1,37 @@
+#include "Time.h"
+
+Time::Time(void) {
+	min = sec = 0;
+}
+Time::Time(int M, int S) {
+	min = M;
+	sec = S;
+}
+Time::Time(const Time&t) {
+	min = t.min;
+	sec = t.sec;
+}
+Time& Time::operator=(const Time& t) {
+	min = t.min;
+	sec = t.sec;
+	return* this;
+}
+
+Time Time::operator*(int l) {
+	int t = min * 60 + sec;
+	t = t * l;
+	Time temp(t / 60, t % 60);
+	return temp;
+}
+
+ostream& operator<<(ostream& out, const Time& t) {
+	out << t.min << " : " << t.sec;
+	return out;
+}
+
+istream& operator>>(istream& in, Time& t) {
+	cout << "\nmin?"; in >> t.min;
+	cout << "\nsec?"; in >> t.sec;
+	return in;
+}
+
